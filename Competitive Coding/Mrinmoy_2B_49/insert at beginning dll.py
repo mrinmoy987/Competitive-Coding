9c1a46c6447f275e8d162916cce6ee5a386a6ce8
mrinmoy987 <mrinmoyapic121@gmail.com>
2025-08-12 16:20:23 +0530

    Update insert at beginning dll.py

--- Competitive Coding/Mrinmoy_2B_49/insert at beginning dll.py
+++ Competitive Coding/Mrinmoy_2B_49/insert at beginning dll.py
@@ -1,45 +1,45 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
+def __init__(self, data):
+self.data = data
+self.prev = None
+self.next = None
 
 class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
+def __init__(self):
+self.head = None
 
-    def append(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            return
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = new_node
-        new_node.prev = last
+def append(self, data):
+new_node = Node(data)
+if not self.head:
+self.head = new_node
+return
+last = self.head
+while last.next:
+last = last.next
+last.next = new_node
+new_node.prev = last
 
 
-    def reverse(self):
-        current = self.head
-        temp = None
+def reverse(self):
+current = self.head
+temp = None
         
         
-        while current:
-            temp = current.prev
-            current.prev = current.next
-            current.next = temp
-            current = current.prev
+while current:
+temp = current.prev
+current.prev = current.next
+current.next = temp
+current = current.prev
         
-        if temp:
-            self.head = temp.prev
+if temp:
+self.head = temp.prev
 
 
-    def print_list(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" <-> " if temp.next else "\n")
-            temp = temp.next
+def print_list(self):
+temp = self.head
+while temp:
+print(temp.data, end=" <-> " if temp.next else "\n")
+temp = temp.next
 dll = DoublyLinkedList()
 dll.append(10)
 dll.append(20)
